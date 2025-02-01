e935db5c8be22ac653cdea9e5e371619103f85c3
Your Name <your.email@example.com>
2025-02-02 04:34:41 +0500

    user hend

--- bot/handlers/users/__init__.py
+++ bot/handlers/users/__init__.py
@@ -1,3 +1,5 @@
+from . import contenttips
+from . import commands
 from . import help
 from . import start
 from . import echo
--- bot/handlers/users/start.py
+++ bot/handlers/users/start.py
@@ -4,6 +4,16 @@ from aiogram.dispatcher.filters.builtin import CommandStart
 from loader import dp
 
 
+# bu ishlidi buning uchun manabu linkni qo'ysa 
+# https://t.me/ecocalculator_bot?start=kunuz
+@dp.message_handler(CommandStart(deep_link='kunuz'))
+async def bot_start(message: types.Message):
+    args = message.get_args()
+    text = f"salom {message.from_user.full_name}!\n"
+    text += f"sizni {args} tafsiya qildi"
+    await message.answer(text)
+
+
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f"Salom, {message.from_user.full_name}!")
