e49b9368fca70744f3d9634942ea829859c97b90
Your Name <your.email@example.com>
2025-02-06 17:33:33 +0500

    anketa

--- button/states/persondata.py
+++ button/states/persondata.py
@@ -1,6 +1,6 @@
 from aiogram.dispatcher.filters.state import StatesGroup, State
 
-class PersanolData(StatesGroup):
-    fullname = State()
-    email = State()
-    phoneNum = State()
+class PersanolData(StatesGroup):  # PersonalData ni to'g'ri aniqlash
+    fullname = State()  # To'liq ism
+    email = State()  # Email
+    phoneNum = State()  # Telefon raqam
