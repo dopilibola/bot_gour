b9bf542e2a1bfe5a4f14bb98ddbbdc5b3831ce95
Your Name <your.email@example.com>
2025-02-06 17:33:33 +0500

    anketa

--- button/handlers/users/help.py
+++ button/handlers/users/help.py
@@ -1,11 +1,19 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandHelp
-
+from states.persondata import PersanolData
 from loader import dp
 
 
-@dp.message_handler(CommandHelp())
+@dp.message_handler(CommandHelp(), state=PersanolData.fullname)
 async def bot_help(message: types.Message):
+    
+    text = ("ismizni kirit iltimos ")
+
+    await message.answer((text))
+
+@dp.message_handler(CommandHelp(), )
+async def bot_help(message: types.Message):
+    
     text = ("Buyruqlar: ",
             "/start - Botni ishga tushirish",
             "/help - Yordam")
